10d59a2e11cd509169d3c042528db6797afda3d8
Julian Alarcon <juaalarconma@gmail.com>
2023-04-20 11:13:50 -0500

    wip:initial | add base project with node, express, esmodules and nodemon

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+app.use(express.json());
+//app.use(controller);
+export default app;
+//# sourceMappingURL=app.js.map
